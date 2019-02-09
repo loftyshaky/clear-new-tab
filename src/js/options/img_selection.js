97a769b3645b1abb7639c1e59052b76f6547627c
loftyshaky <loftyshaky@gmail.com>
2019-02-10 09:46:55 +1000

    Don't select image when clicking on img_btn in it

--- src/js/options/img_selection.js
+++ src/js/options/img_selection.js
@@ -12,12 +12,13 @@ configure({ enforceActions: 'observed' });
 //> select image when clicking on it
 export const select_img = async (clicked_img_id, e) => {
     try {
-        const clicked_img_page_i = img_i.get_img_i_by_id(clicked_img_id);
-        const clicked_img_i = clicked_img_page_i + img_i.determine_img_i_modificator();
         const not_img_preview = !x.matches(e.target, '.img_preview');
         const not_delete_img_btn = !x.matches(x.closest(e.target, '.delete_img_btn'), '.delete_img_btn');
+        const not_img_btn = !x.matches(e.target, '.img_btn');
 
-        if (not_img_preview && not_delete_img_btn) {
+        if (not_img_preview && not_delete_img_btn && not_img_btn) {
+            const clicked_img_page_i = img_i.get_img_i_by_id(clicked_img_id);
+            const clicked_img_i = clicked_img_page_i + img_i.determine_img_i_modificator();
             change_selected_img(clicked_img_id, clicked_img_page_i);
             const ed_all = await eda();
             const img = await x.send_message_to_background_c({ message: 'get_img_obj_when_selecting_on_it', i: clicked_img_i });
