5470a2dba0bad1a903d08caaf0db10acaf3279a6
loftyshaky <loftyshaky@gmail.com>
2019-02-14 07:37:52 +1000

    Higlight elements only when tab key pressed

--- src/js/tab_focus.js
+++ src/js/tab_focus.js
@@ -1,8 +1,13 @@
 import x from 'x';
 
-const set_using_mouse_cls = fun_name => {
+const set_using_mouse_cls = (fun_name, e) => {
     try {
-        x[fun_name](document.body, 'using_mouse');
+        const tab_or_mouse_btn_pressed = e && e.keyCode ? e.keyCode === 9 : true;
+        const esc_pressed = e && e.keyCode ? e.keyCode === con.esc_key_code : false;
+
+        if (tab_or_mouse_btn_pressed && !esc_pressed) {
+            x[fun_name](document.body, 'using_mouse');
+        }
 
     } catch (er) {
         err(er, 214);
@@ -11,10 +16,12 @@ const set_using_mouse_cls = fun_name => {
 
 const prevent_el_focus_on_esc = e => {
     try {
-        const esc_pressed = e.keyCode === 27;
+        const esc_pressed = e.keyCode === con.esc_key_code;
 
         if (esc_pressed) {
             document.activeElement.blur();
+
+            set_using_mouse_cls('add_cls');
         }
 
     } catch (er) {
@@ -25,3 +32,7 @@ const prevent_el_focus_on_esc = e => {
 x.bind(document.body, 'mousedown', set_using_mouse_cls.bind(null, 'add_cls'));
 x.bind(document.body, 'keydown', prevent_el_focus_on_esc);
 x.bind(document.body, 'keydown', set_using_mouse_cls.bind(null, 'remove_cls'));
+
+const con = {
+    esc_key_code: 27,
+};
