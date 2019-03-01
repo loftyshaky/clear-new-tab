3b209a02174ad2133266c96eebbf71efbab77821
loftyshaky <loftyshaky@gmail.com>
2019-03-01 19:19:06 +1000

    Fix image not changing on theme install

--- src/js/background/theme_img.js
+++ src/js/background/theme_img.js
@@ -129,7 +129,7 @@ export const get_theme_img = async (theme_id, reinstall_even_if_theme_img_alread
 
             await db.ed.update(1, { current_img: new_current_img });
             await get_new_future_img.get_new_future_img(new_current_img + 1);
-            imgs.preload_current_and_future_img('reload');
+            await imgs.preload_current_and_future_img('reload');
 
             x.iterate_all_tabs(x.send_message_to_tab, [{ message: 'change_current_img_input_val' }]);
             x.iterate_all_tabs(x.send_message_to_tab, [{ message: 'reload_img' }]);
