6407e4fc1c20ec889a4dd89d4f776df6b18371d9
loftyshaky <loftyshaky@gmail.com>
2019-03-18 19:02:07 +1000

    Fix images overlaped by fillers

--- src/js/options/scrolling.js
+++ src/js/options/scrolling.js
@@ -1,36 +1,36 @@
 import { observable, action, configure } from 'mobx';
 
-import * as prevent_scrolling from 'js/prevent_scrolling';
-
 configure({ enforceActions: 'observed' });
 
 //> show / hide backgrounds_fieldset_fillers
 export const show_or_hide_backgrounds_fieldset_fillers = action(() => {
     try {
-        const scroll_top = mut.backgrounds_fieldset.scrollTop;
-        const scroll_height = mut.backgrounds_fieldset.scrollHeight - mut.backgrounds_fieldset.clientHeight;
-        const at_the_top_of_backgrounds_fieldset = scroll_top !== 0;
-        const at_the_top_of_backgrounds_fieldset_scrolling_up = scroll_top <= 15 && prevent_scrolling.mut.delta_y < 0;
-        const at_the_bottom_of_backgrounds_fieldset_scrolling_down = scroll_top >= scroll_height - 15 && prevent_scrolling.mut.delta_y > 0;
-        const not_at_the_top_of_backgrounds_fieldset_scrolling_up = scroll_top <= scroll_height - 15 && prevent_scrolling.mut.delta_y < 0;
-        const not_at_the_bottom = scroll_top <= scroll_height - 15;
-        const no_scrollbar = scroll_top === 0;
+        const scrollbar_is_visible = mut.backgrounds_fieldset.scrollHeight > mut.backgrounds_fieldset.clientHeight;
 
-        if (at_the_top_of_backgrounds_fieldset) {
-            ob.backgrounds_fieldset_filler_top_none_cls = '';
+        if (scrollbar_is_visible) {
+            const scroll_top = mut.backgrounds_fieldset.scrollTop;
+            const scroll_height = mut.backgrounds_fieldset.scrollHeight - mut.backgrounds_fieldset.clientHeight;
+            const at_the_top_of_backgrounds_fieldset = scroll_top === 0;
+            const at_the_bottom_of_backgrounds_fieldset = scroll_top >= scroll_height - 15;
 
-        } else if (at_the_top_of_backgrounds_fieldset_scrolling_up || no_scrollbar) {
+
+            if (at_the_top_of_backgrounds_fieldset) {
+                ob.backgrounds_fieldset_filler_top_none_cls = 'none';
+
+            } else {
+                ob.backgrounds_fieldset_filler_top_none_cls = '';
+            }
+
+            if (at_the_bottom_of_backgrounds_fieldset) {
+                ob.backgrounds_fieldset_filler_bottom_none_cls = 'none';
+
+            } else {
+                ob.backgrounds_fieldset_filler_bottom_none_cls = '';
+            }
+
+        } else {
             ob.backgrounds_fieldset_filler_top_none_cls = 'none';
-
-        } else if (at_the_bottom_of_backgrounds_fieldset_scrolling_down) {
-            ob.backgrounds_fieldset_filler_top_none_cls = '';
             ob.backgrounds_fieldset_filler_bottom_none_cls = 'none';
-
-        } else if (not_at_the_top_of_backgrounds_fieldset_scrolling_up) {
-            ob.backgrounds_fieldset_filler_bottom_none_cls = '';
-
-        } else if (not_at_the_bottom) {
-            ob.backgrounds_fieldset_filler_bottom_none_cls = '';
         }
 
     } catch (er) {
--- src/js/prevent_scrolling.js
+++ src/js/prevent_scrolling.js
@@ -4,10 +4,10 @@ export const prevent_scrolling = function prevent_scrolling(e) {
             const scroll_height = this.scrollHeight;
             const client_height = this.clientHeight;
             const scroll_top = this.scrollTop;
-            mut.delta_y = e.deltaY;
+            const delta_y = e.deltaY;
             const container_is_scrollable = scroll_height > client_height;
-            const at_the_top_of_container = mut.delta_y < 0 && scroll_top === 0;
-            const at_the_bottom_of_container = mut.delta_y > 0 && scroll_top === scroll_height - client_height;
+            const at_the_top_of_container = delta_y < 0 && scroll_top === 0;
+            const at_the_bottom_of_container = delta_y > 0 && scroll_top === scroll_height - client_height;
 
             if (container_is_scrollable && (at_the_top_of_container || at_the_bottom_of_container)) {
                 e.preventDefault();
@@ -17,8 +17,4 @@ export const prevent_scrolling = function prevent_scrolling(e) {
     } catch (er) {
         err(er, 183);
     }
-};
-
-export const mut = {
-    delta_y: 3,
 };
