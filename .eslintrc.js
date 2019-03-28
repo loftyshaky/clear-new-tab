2866ddd8e119dd635fb5db6d9650687c1fef84bd
loftyshaky <loftyshaky@gmail.com>
2019-03-29 00:28:27 +1000

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     rules: {
-        'camelcase': 0,
+        camelcase: 0,
         'max-len': 0,
         'object-curly-newline': 0,
         'import/no-extraneous-dependencies': 0,
@@ -18,20 +18,20 @@ module.exports = {
         'jsx-a11y/anchor-has-content': 0,
         'jsx-a11y/anchor-is-valid': ['error',
             {
-                'components': ['Link']
+                components: ['Link']
             },
         ],
-        "no-restricted-syntax": [
-            "error",
-            "ForInStatement",
-            "LabeledStatement",
-            "WithStatement"
+        'no-restricted-syntax': [
+            'error',
+            'ForInStatement',
+            'LabeledStatement',
+            'WithStatement'
         ],
-        'indent': [
+        indent: [
             'error',
             4,
             {
-                "SwitchCase": 1,
+                SwitchCase: 1,
             }
         ],
         'react/jsx-indent': [
@@ -49,64 +49,61 @@ module.exports = {
         'no-use-before-define': [
             'error',
             {
-                'variables': false,
+                variables: false,
             },
         ],
         'arrow-parens': [
             'error',
             'as-needed',
         ],
-        'radix': [
+        radix: [
             'error',
             'as-needed',
         ],
         'no-underscore-dangle': [
             'error',
             {
-                'allow':
+                allow:
                     ['__'],
             },
         ],
     },
 
     globals: {
-        'window': false,
-        'document': false,
-        'Image': false,
-        'File': false,
-        'NodeList': false,
-        'MutationObserver': false,
-        'xcon': false,
-        'ed': false,
-        'ed': false,
-        'eda': false,
-        'err': false,
-        't': false,
-        'er_obj': false,
-        'l': false,
-        'what_browser': false,
-        'browser': false,
-        'page': false,
-        's': false,
-        'sa': false,
-        'sb': false,
-        'sab': false,
-        'set_default_settings': false,
+        window: false,
+        document: false,
+        MutationObserver: false,
+        xcon: false,
+        ed: false,
+        ed: false,
+        eda: false,
+        err: false,
+        t: false,
+        er_obj: false,
+        l: false,
+        what_browser: false,
+        browser: false,
+        page: false,
+        s: false,
+        sa: false,
+        sb: false,
+        sab: false,
+        set_default_settings: false,
     },
 
     settings: {
         'import/resolver':
         {
-            'webpack':
+            webpack:
             {
-                'config': 'webpack.shared.js',
+                config: 'webpack.shared.js',
             },
         },
     },
 
-    'parser': 'babel-eslint',
-    'extends': 'airbnb',
-    'plugins': [
+    parser: 'babel-eslint',
+    extends: 'airbnb',
+    plugins: [
         'react',
         'jsx-a11y',
         'import'
