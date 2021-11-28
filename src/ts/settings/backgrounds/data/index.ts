a571287ce4d9a0a676d3f188dcf3014909cc7f1a
loftyshaky <loftyshaky@gmail.com>
2021-11-28 12:46:58 +1000

    refactor: change import order

--- src/ts/settings/backgrounds/data/index.ts
+++ src/ts/settings/backgrounds/data/index.ts
@@ -1,3 +1,3 @@
 export * from 'settings/backgrounds/data/main';
-export * from 'settings/backgrounds/data/virtualized_list';
 export * from 'settings/backgrounds/data/upload';
+export * from 'settings/backgrounds/data/virtualized_list';
