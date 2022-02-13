9f17b910d1eb9fc97261d4078f797b0686b0f0f7
loftyshaky <loftyshaky@gmail.com>
2022-02-13 20:47:16 +1000

    feat: stop slideshow on new tab close

--- src/ts/new_tab/init.ts
+++ src/ts/new_tab/init.ts
@@ -10,4 +10,7 @@ export const init = (): Promise<void> =>
             'visibilitychange',
             d_background.BackgroundChange.i().react_to_visibility_change,
         );
+        x.bind(window, 'beforeunload', () => {
+            ext.send_msg({ msg: 'clear_slideshow_timer' });
+        });
     }, 'cnt_61125');
