f1890d96b56661657e137c372f38e82f47d75b72
loftyshaky <loftyshaky@gmail.com>
2022-02-16 03:43:53 +1000

    fix: set settings on extension install

--- src/ts/background/init.ts
+++ src/ts/background/init.ts
@@ -3,5 +3,6 @@ import { db, s_data } from 'shared/internal';
 export const init = (): Promise<void> =>
     err_async(async () => {
         s_data.Main.i().init_defaults();
+        await s_data.Main.i().set_from_storage();
         db.init_db();
     }, 'cnt_1016');
