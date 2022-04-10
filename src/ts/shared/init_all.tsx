65ca17a69aa21534e498c00d7b54f9e0898f59af
loftyshaky <loftyshaky@gmail.com>
2022-04-10 18:20:29 +1000

    Revert "fix(settings): wrong section and input width"
    This reverts commit 3d80175a8fca04d9b75ad067cf75b03929ef7da1.

--- src/ts/shared/init_all.tsx
+++ src/ts/shared/init_all.tsx
@@ -134,16 +134,11 @@ export class InitAll {
                         'settings/internal'
                     );
 
-                    self.setTimeout(() => {
-                        err_async(async () => {
-                            await d_inputs.InputWidth.i().calculate_for_all_sections({
-                                sections: d_sections.Main.i().sections as i_inputs.Sections,
-                                all_sections_inputs_equal_width: true,
-                            });
-                            d_sections.Width.i().set();
-                        }, 'cnt_56747');
-                    }, data.settings.transition_duration);
-
+                    await d_inputs.InputWidth.i().calculate_for_all_sections({
+                        sections: d_sections.Main.i().sections as i_inputs.Sections,
+                        all_sections_inputs_equal_width: true,
+                    });
+                    d_sections.Width.i().set();
                     d_scheduler.Position.i().set_left();
                     await d_scheduler.Val.i().set_add_new_task_btn_ability();
 
