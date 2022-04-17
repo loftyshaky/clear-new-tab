b6ac4c46d3c64a0707367afba5dae463fceb1610
loftyshaky <loftyshaky@gmail.com>
2022-04-17 10:28:59 +1000

    refactor: remove unneeded code

--- src/ts/settings/browser_theme/scripts/theme_id.ts
+++ src/ts/settings/browser_theme/scripts/theme_id.ts
@@ -23,20 +23,7 @@ export class ThemeId {
                     err(() => item.type === 'theme' && item.enabled, 'cnt_65648'),
             );
 
-            // eslint-disable-next-line max-len
-            const enabled_themes_but_not_with_id_of_last_installed_theme: Management.ExtensionInfo[] =
-                enabled_themes.filter((item: Management.ExtensionInfo): boolean =>
-                    err(() => item.id !== data.settings.id_of_last_installed_theme, 'cnt_65368'),
-                );
-
             const found_enabled_themes = !_.isEmpty(enabled_themes);
-            const found_enabled_theme_with_last_installed_id = !_.isEmpty(
-                enabled_themes_but_not_with_id_of_last_installed_theme,
-            );
-
-            if (found_enabled_theme_with_last_installed_id) {
-                return enabled_themes_but_not_with_id_of_last_installed_theme[0].id;
-            }
 
             if (found_enabled_themes) {
                 return enabled_themes[0].id;
