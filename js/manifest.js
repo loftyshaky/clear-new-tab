48ae65f94be98cbe5647003e6ec80e25f5968bb7
loftyshaky <loftyshaky@gmail.com>
2022-07-12 21:42:27 +1000

    feat: remove extension name from en messages

--- js/manifest.js
+++ js/manifest.js
@@ -10,7 +10,7 @@ class Manifest {
     generate = ({ test, browser }) => {
         const manifest = {
             manifest_version: 3,
-            name: '__MSG_name__',
+            name: 'Clear New Tab',
             description: '__MSG_description__',
             background: {
                 service_worker: 'background.js',
